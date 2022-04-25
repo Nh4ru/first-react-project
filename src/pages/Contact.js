de05050ac4df77d334ac0d00ec3e9aba13f1c283
Nh4ru <tristanfernandez@live.fr>
2022-04-25 12:18:27 +0200

    Add react-router-dom & configure the routing for Home, Portfolio, Contact pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,19 @@
+const Contact = () => {
+    return(
+        <section>
+            <div className="container-fluid">
+                <div className="row">
+                    <h1>Contact</h1>
+                </div>
+            </div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et autem perferendis dolorum ipsa veritatis animi eveniet aspernatur, asperiores, odio sunt nihil numquam facere nisi quibusdam quaerat distinctio molestias voluptatem magnam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et autem perferendis dolorum ipsa veritatis animi eveniet aspernatur, asperiores, odio sunt nihil numquam facere nisi quibusdam quaerat distinctio molestias voluptatem magnam.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Contact
